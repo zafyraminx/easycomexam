2255a9ce16ee483152ec73dd03dbb2ca4063932b
zafyraminx <80800798+michael-zafra-da5@users.noreply.github.com>
2023-01-16 22:17:20 +0800

    Initial Commit

---
+++ Easycom Japan PH/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Easycom Japan PH
+//
+//  Created by Michael Angelo Zafra on 1/16/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
